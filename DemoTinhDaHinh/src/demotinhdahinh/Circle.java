353f2c1ee59465b3e2d895cbcd6fee0f2a123b69
Trần Trung <nguyentrantrung1986@gmail.com>
2016-02-17 15:49:42 -0800

    demo TinhDaHinh in Circle class

---
+++ DemoTinhDaHinh/src/demotinhdahinh/Circle.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package demotinhdahinh;
+
+/**
+ *
+ * @author TrungNT
+ */
+public class Circle {
+    public void Method1()
+    {
+    }
+    
+    public void Method2()
+    {
+        System.out.println("Day la phuong thuc 2 trong class Circle");
+    }
+    
+    public void Method3(int a)
+    {
+        System.out.println("Day la phuong thuc 3 trong class Circle co du lieu: " + a);
+    }
+    
+    public void Method4(int a, int b)
+    {
+        System.out.println("Day la phuong thuc 4 trong class Circle co du lieu: " + a + " va " + b);
+    }
+    
+    public void Method5(String a)
+    {
+        System.out.println("Day la phuong thuc 5 trong class Circle co du lieu " + a);
+    }
+}
---
+++ DemoTinhDaHinh/src/demotinhdahinh/DemoTinhDaHinh.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package demotinhdahinh;
+
+/**
+ *
+ * @author TrungNT
+ */
+public class DemoTinhDaHinh {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Circle oCircle = new Circle();
+        oCircle.Method2();
+        oCircle.Method3(3);
+        oCircle.Method4(5, 6);
+        oCircle.Method5("Nguyen Tran Trung");
+    }
+    
+}
